41b1d4bd0b883ed4a19ecb7e54c8526f69abaed1
Chris Shaw <shaw64489@yahoo.com>
2018-11-12 14:19:43 -0500

    insert video to videos table

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -1,47 +1,54 @@
 <?php
-
 class VideoProcessor {
 
     private $con;
-    private $sizeLimit = 500000000; //.5GB
+    private $sizeLimit = 500000000;
     private $allowedTypes = array("mp4", "flv", "webm", "mkv", "vob", "ogv", "ogg", "avi", "wmv", "mov", "mpeg", "mpg");
 
     public function __construct($con) {
-
         $this->con = $con;
-        
     }
 
     public function upload($videoUploadData) {
 
         $targetDir = "uploads/videos/";
         $videoData = $videoUploadData->videoDataArray;
-
+        
         $tempFilePath = $targetDir . uniqid() . basename($videoData["name"]);
+        //uploads/videos/5aa3e9343c9ffdogs_playing.flv
 
         $tempFilePath = str_replace(" ", "_", $tempFilePath);
 
         $isValidData = $this->processData($videoData, $tempFilePath);
 
-        if (!$isValidData) {
+        if(!$isValidData) {
             return false;
         }
 
-        if (move_uploaded_file($videoData["tmp_name"], $tempFilePath)) {
-            echo "File moved successfully";
+        if(move_uploaded_file($videoData["tmp_name"], $tempFilePath)) {
+            
+            $finalFilePath = $targetDir . uniqid() . ".mp4";
+
+            if(!$this->insertVideoData($videoUploadData, $finalFilePath)) {
+                echo "Insert query failed";
+                return false;
+            }
+
         }
     }
 
     private function processData($videoData, $filePath) {
-        $videoType = pathinfo($filePath, PATHINFO_EXTENSION);
-
+        $videoType = pathInfo($filePath, PATHINFO_EXTENSION);
+        
         if(!$this->isValidSize($videoData)) {
-            echo "File too large Cant be more than " . $this->sizeLimit . " bytes.";
+            echo "File too large. Can't be more than " . $this->sizeLimit . " bytes";
             return false;
-        } else if (!$this->isValidType($videoType)) {
-            echo "Invalid file type.";
+        }
+        else if(!$this->isValidType($videoType)) {
+            echo "Invalid file type";
             return false;
-        } else if ($this->hasError($videoData)) {
+        }
+        else if($this->hasError($videoData)) {
             echo "Error code: " . $videoData["error"];
             return false;
         }
@@ -57,10 +64,23 @@ class VideoProcessor {
         $lowercased = strtolower($type);
         return in_array($lowercased, $this->allowedTypes);
     }
-
+    
     private function hasError($data) {
         return $data["error"] != 0;
     }
 
+    private function insertVideoData($uploadData, $filePath) {
+        $query = $this->con->prepare("INSERT INTO videos(title, uploadedBy, description, privacy, category, filePath)
+                                        VALUES(:title, :uploadedBy, :description, :privacy, :category, :filePath)");
+
+        $query->bindParam(":title", $uploadData->title);
+        $query->bindParam(":uploadedBy", $uploadData->uploadedBy);
+        $query->bindParam(":description", $uploadData->description);
+        $query->bindParam(":privacy", $uploadData->privacy);
+        $query->bindParam(":category", $uploadData->category);
+        $query->bindParam(":filePath", $filePath);
+
+        return $query->execute();
+    }
 }
 ?>
